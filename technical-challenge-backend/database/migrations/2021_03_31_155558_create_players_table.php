f08bab0b4121755c73970ba6275b93bedddbf85e
Tim <ryalin1286@gmail.com>
2021-03-31 17:05:59 +0100

    Created a Player, Game and Round model and related database migration. Rounds have many Games, and Games have many Players. Other than the foreign keys, I've mirrored the data currently being used by each object in the React frontend app. Players have won, score and name propertiies. Games have id, deuce, service and complete properties. Rounds have id and complete properties.

---
+++ technical-challenge-backend/database/migrations/2021_03_31_155558_create_players_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("game_id")
+                ->constrained()
+                ->onDelete("cascade");
+            $table->string("name", 255);
+            $table->integer('score');
+            $table->tinyInteger('won');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
