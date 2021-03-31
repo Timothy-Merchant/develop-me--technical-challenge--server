cae57ab5d238e0fc73e33a76fda5e3c856cd5d2e
Tim <ryalin1286@gmail.com>
2021-03-31 15:59:00 +0100

    Added commented out provisional endpoint list copied over from the frontend markdown file, for reference (added to api routes file).

--- technical-challenge-backend/routes/api.php
+++ technical-challenge-backend/routes/api.php
@@ -14,6 +14,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// <!-- Post Players -->
+// POST api/players
+// <!-- This will then create rounds and games -->
+
+// Get Rounds
+// <!-- Get all rounds -->
+// GET api/rounds
+// <!-- Get a single round -->
+// GET api/rounds/1
+// <!-- Get all games for a single round -->
+// GET api/rounds/1/games
+// <!-- Get a single game -->
+// GET api/rounds/1/games/1
+// <!-- Get players for a specific game -->
+// GET api/rounds/1/games/1/players
+
+// One to Many relationships:
+
+// Rounds (Have Many Games) 
+// Games (Have Many Players)
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
