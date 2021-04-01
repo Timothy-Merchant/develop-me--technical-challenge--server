4c61ade76a273db11826426f5eede8e2bd160298
Tim <ryalin1286@gmail.com>
2021-04-01 09:40:26 +0100

    Created CRUD functions for the RoundController. Some of these may be unnecessary later, but I have added all for testing purposes for now.

--- technical-challenge-backend/app/Http/Controllers/RoundController.php
+++ technical-challenge-backend/app/Http/Controllers/RoundController.php
@@ -2,9 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Round;
 use Illuminate\Http\Request;
 
 class RoundController extends Controller
 {
-    //
+    public function index()
+    {
+        $rounds = Round::all();
+        return $rounds;
+    }
+
+    public function show(Round $round)
+    {
+        // just return the round
+        return $round;
+    }
+
+    public function store(Request $request)
+    {
+        // get all the request data
+        // returns an array of all the data the user sent
+        $data = $request->all();
+        // create round with data and store in DB
+        // and return it as JSON
+        // automatically gets 201 status as it's a POST request
+        return Round::create($data);
+    }
+
+    public function destroy(Round $round)
+    {
+        // delete the article from the DB
+        $round->delete();
+        // use a 204 code as there is no content in the response
+        return response(null, 204);
+    }
+
+    public function update(Request $request, Round $round)
+    {
+        // get the request data
+        $data = $request->all();
+        // update the round using the fill method
+        // then save it to the database
+        $round->fill($data)->save();
+        // return the updated version
+        return $round;
+    }
 }
--- technical-challenge-backend/routes/api.php
+++ technical-challenge-backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RoundController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,17 +20,22 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 // Create Players for the game (from the frontend Roster list)
-Route::post('/players', [PlayerController::class, 'create']);
+Route::post('/players', [PlayerController::class, 'store']);
 
 // Routes that are based on rounds
 Route::group(["prefix" => "rounds"], function () {
     Route::get("/", [RoundController::class, "index"]);
+    Route::post("/", [RoundController::class, "store"]);
+    Route::put("/", [RoundController::class, "update"]);
+    Route::delete("/", [RoundController::class, "destroy"]);
     Route::get("/{round}", [RoundController::class, "show"]);
-    Route::get("/{round}/games", [GameController::class, "index"]);
-    Route::get("/{round}/games/{game}", [GameController::class, "show"]);
-    Route::get("/{round}/games/{game}/players", [PlayerController::class, "index"]);
-    Route::get("/{round}/games/{game}/players/{player}", [PlayerController::class, "show"]);
+    Route::group(["prefix" => "{round}"], function () {
+        Route::get("/games", [GameController::class, "index"]);
+        Route::get("/games/{game}", [GameController::class, "show"]);
+        Route::get("/games/{game}/players", [PlayerController::class, "index"]);
+        Route::get("/games/{game}/players/{player}", [PlayerController::class, "show"]);
+    });
     // Route::group(["prefix" => "{round}"], function () {
-        
+
     // });
 });
