c8a060b770182a458aab9aff6f501d230cf398d6
Tim <ryalin1286@gmail.com>
2021-04-09 08:42:54 +0100

    Finally solved the serverside issues regarding being unable to use the deployed APi with the app, it was due to the serverside .env file not being updated with the correct information.

--- technical-challenge-backend/routes/api.php
+++ technical-challenge-backend/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GameController;
 use App\Http\Controllers\RoundController;
 use App\Http\Controllers\PlayerController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\TournamentController;
 
 /*
